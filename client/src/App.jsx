191c347ce0b10ba7b668a2872b417155dc681320
Bayz.ng <ayofe70@gmail.com>
2024-02-18 15:17:09 -0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
+import Home from "./pages/Home";
+import HomePage from "./components/HomePage";
+
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+     <Routes>
+          <Route path="/" element={ <HomePage/>}/>
+          <Route path="/Wallet" element={ <Home/> }/>
+     </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
+
+
+
